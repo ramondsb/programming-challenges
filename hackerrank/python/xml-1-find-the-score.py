84f4c76c7a5830e3c2166fa88881b94e96287ebc
Ramon Brito <git@ramondsb.com>
2020-10-13 23:39:02 -0400

    [hackerrank] solve xml-1-find-the-score

---
+++ hackerrank/python/xml-1-find-the-score.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Link: https://www.hackerrank.com/challenges/xml-1-find-the-score
+
+
+import sys
+import xml.etree.ElementTree as etree
+
+def get_attr_number(node):
+    score = len(node.attrib)
+    for child in node:
+       score += get_attr_number(child)
+
+    return score
+
+
+if __name__ == '__main__':
+    sys.stdin.readline()
+    xml = sys.stdin.read()
+    tree = etree.ElementTree(etree.fromstring(xml))
+    root = tree.getroot()
+    print(get_attr_number(root))
